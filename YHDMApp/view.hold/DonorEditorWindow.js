13f04eee61eb16eb0c0f6bdff17e9ee448411a25
mrljp <lpgit@pollackworld.us>
2013-07-11 10:56:04 -0700

    Remove YHDMApp folder. Didn't need to be checked in.

--- YHDMApp/view.hold/DonorEditorWindow.js
+++
@@ -1,8 +0,0 @@
-Ext.define('YHDMApp.view.DonorEditorWindow', {
-    extend   : 'Ext.Window',
-    alias  : 'widget.donorEditorWindow',
-    title   : 'Yom Hashoah Donor Management',
-    initComponent : function() {
-        console.log('In DonorEditorWindow');
-    }
-});
